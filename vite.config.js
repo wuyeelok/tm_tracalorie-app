5cd88b4aacbc815a2fb1df44a82e725622245410
wuyeelok <wuyeelok@gmail.com>
2024-03-24 22:45:59 +0800

    Setup: Update project name and port

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: resolve(__dirname, "src/"),
-  base: "/beyond-css-website-starter-template-v2/",
+  base: "/tm_tracalorie-app/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
