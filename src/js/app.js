ef507853aba1f72f784851829463e1ee5ac20177
wuyeelok <wuyeelok@gmail.com>
2024-03-25 10:09:22 +0800

    Class: Meal

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
 console.log("Tracalorie!");
+
+class Meal {
+  #id;
+  #name;
+  #calories;
+
+  constructor(id, name, calories) {
+    this.#id = id;
+    this.#name = name;
+    this.#calories = calories;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get calories() {
+    return this.#calories;
+  }
+}
