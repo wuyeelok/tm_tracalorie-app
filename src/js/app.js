ab954629e7da3c4eb9368f4499c34587d8bb95de
wuyeelok <wuyeelok@gmail.com>
2024-03-25 10:11:19 +0800

    Class: Workout

--- src/js/app.js
+++ src/js/app.js
@@ -23,3 +23,27 @@ class Meal {
     return this.#calories;
   }
 }
+
+class Workout {
+  #id;
+  #name;
+  #calories;
+
+  constructor(id, name, calories) {
+    this.#id = id;
+    this.#name = name;
+    this.#calories = calories;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get calories() {
+    return this.#calories;
+  }
+}
